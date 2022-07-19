e44e04a2a0020d525ca6810de62de0129bfda461
Michael Okello <okellofrost@gmail.com>
2022-07-19 18:39:13 +0300

    finalising

--- circle.js
+++ circle.js
@@ -1,10 +1,11 @@
 const _radius = new WeakMap();
 
-class Circle{
+export class Circle{
     constructor(radius){
- _radius.set(this, radius);
+    _radius.set(this, radius);
     }
     draw (){
         console.log('Circle with radius'+ _radius.get(this));
     }
 }
+
